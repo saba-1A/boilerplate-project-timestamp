c9152da2f2def57a95b94e638b5581eb51e2fa30
saba-1A <sabafathima789nazar@gmail.com>
2025-06-20 23:03:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,55 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  res.json({ greeting: 'hello API' });
 });
 
 function unixToUTC(unixTime) {
-  const convertedDate = new Date(unixTime)
-  return convertedDate.toUTCString()
+  const convertedDate = new Date(unixTime);
+  return convertedDate.toUTCString();
 }
 
 function getCurrentUnixTime() {
-    const currentUnixTime = Date.now()
-    return {"unix": Number(currentUnixTime), "utc": unixToUTC(currentUnixTime)}
+  const currentUnixTime = Date.now();
+  return { "unix": currentUnixTime, "utc": unixToUTC(currentUnixTime) };
 }
 
 function returnJson(date) {
   if (!date) {
-    return getCurrentUnixTime()
+    return getCurrentUnixTime();
   } else {
-    let requestedDate = ""
-    if (date > 0) {
-      requestedDate = new Date(Number(date))
+    let requestedDate;
+
+    if (/^\d+$/.test(date)) {
+      requestedDate = new Date(Number(date)); // Handle Unix timestamp
     } else {
-      requestedDate = new Date(date)
+      requestedDate = new Date(date); // Handle ISO or other string date
     }
-    const requestedUnixDate = requestedDate.getTime()
-    const requestedUtcDate = requestedDate.toUTCString()
-    if (!requestedUnixDate) {return { error : "Invalid Date" }}
-    return {"unix": Number(requestedDate.getTime()), "utc": requestedUtcDate}
+
+    if (requestedDate.toString() === "Invalid Date") {
+      return { error: "Invalid Date" };
+    }
+
+    return {
+      unix: requestedDate.getTime(),
+      utc: requestedDate.toUTCString()
+    };
   }
 }
 
 app.get('/api/:date?', (req, res) => {
-  res.json(returnJson(req.params.date))
-})
+  res.json(returnJson(req.params.date));
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
